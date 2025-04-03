ad36b75ca9a4132912cc89ea5a08e371407526f4
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-03 15:30:44 -0300

    feat: middleware de autenticação JWT

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,1 +1,20 @@
-// Middleware de autenticaÁ„o
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = require("../config/dotenv")
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization")
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Acesso negado. Token n√£o fornecido." })
+
+  try {
+    const decoded = jwt.verify(token.replace("Bearer ", ""), JWT_SECRET)
+    req.user = decoded
+    next()
+  } catch (error) {
+    res.status(400).json({ message: "Token inv√°lido." })
+  }
+}
+
+module.exports = authMiddleware
