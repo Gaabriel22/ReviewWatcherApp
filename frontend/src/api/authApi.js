21b36b6d64eaf7689c5a7ed892e89311ae4a7452
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-04-06 10:23:53 -0300

    Instalação do lucide-react que faltava, e ajuste das variaveis no arquivo authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,11 +1,11 @@
 import axios from "./axios"
 
-export const login = async (credentials) => {
+export const loginUser = async (credentials) => {
   const response = await axios.post("/auth/login", credentials)
   return response.data
 }
 
-export const register = async (userData) => {
+export const registerUser = async (userData) => {
   const response = await axios.post("/auth/register", userData)
   return response.data
 }
